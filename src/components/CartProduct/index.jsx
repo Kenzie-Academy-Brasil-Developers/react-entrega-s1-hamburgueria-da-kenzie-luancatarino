afb492235e6c818724328969d88adfc722847ad8
luancatarino <luancatarino@gmail.com>
2022-04-06 11:21:23 -0300

    fix: Button CSS

--- src/components/CartProduct/index.jsx
+++ src/components/CartProduct/index.jsx
@@ -1,31 +1,28 @@
 import "./index.css";
-import {useState} from "react"
 
-
-function CartProduct ({product, currentSale, setCurrentSale}){
-       
-    function removeProduct () {
-        const productRemoved = currentSale.filter(item => item.id !== product.id )
-        setCurrentSale(productRemoved)
+function CartProduct({ product, currentSale, setCurrentSale }) {
+    function removeProduct() {
+        const productRemoved = currentSale.filter((item) => item.id !== product.id);
+        setCurrentSale(productRemoved);
     }
-
 
     return (
         <>
-        <div className="cartProduct">
-            <div className="cartProductImg">
-                <img src={product.img} alt={product.name}/>
+            <div className="cartProduct">
+                <div className="cartProductImg">
+                    <img src={product.img} alt={product.name} />
+                </div>
+                <div className="cartProductDetails">
+                    <h2>{product.name}</h2>
+                    <p>{product.category}</p>
+                </div>
+                <p className="cartProductPrice">R$ {product.price.toFixed(2)}</p>
+                <button className="removeCartProduct" onClick={removeProduct}>
+                    Remover
+                </button>
             </div>
-            <div className="cartProductDetails">
-                <h2>{product.name}</h2>
-                <p>{product.category}</p>
-            </div>
-            <p className="cartProductPrice">R$ {product.price.toFixed(2)}</p>
-            <button className="removeCartProduct" onClick={removeProduct}>Remover</button>
-        </div>
-     
         </>
-    )    
+    );
 }
 
-export default CartProduct
+export default CartProduct;
